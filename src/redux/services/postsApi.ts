c8f5d19964b01272696b200bb9f65210232842a6
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-04 15:44:01 +0600

    added other api crud operations

--- src/redux/services/postsApi.ts
+++ src/redux/services/postsApi.ts
@@ -3,14 +3,40 @@ import { Post } from "../features/posts";
 
 export const postsApi = createApi({
   reducerPath: 'postApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/posts' }),
   tagTypes: ['Posts'],
   endpoints: (builder) => ({
     getPosts: builder.query<Post[], void>({
-      query: () => 'posts',
+      query: () => '/',
       providesTags: [{ type: 'Posts', id: 'LIST' }],
-    })
+    }),
+
+    createPost: builder.mutation({
+      query: (title) => ({
+        url: '/',
+        method: 'POST',
+        body: { title },
+      }),
+      invalidatesTags: ['Posts'],
+    }),
+
+    updatePost: builder.mutation({
+      query: ({ id, title }) => ({
+        url: `/${id}`,
+        method: 'PUT',
+        body: { title },
+      }),
+      invalidatesTags: ['Posts'],
+    }),
+
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Posts'],
+    }),
   })
 })
 
-export const { useGetPostsQuery } = postsApi;
+export const { useGetPostsQuery, useCreatePostMutation, useDeletePostMutation, useUpdatePostMutation } = postsApi;
